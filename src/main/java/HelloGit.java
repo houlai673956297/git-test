fe844a9756c6278646e3330cb94b616d09d239c7
houlai <673956297@qq.com>
2022-04-17 15:58:28 +0800

    third commit

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -2,5 +2,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("HelloGIt");
         System.out.println("HelloGIt2");
+        System.out.println("HelloGIt3");
     }
 }
