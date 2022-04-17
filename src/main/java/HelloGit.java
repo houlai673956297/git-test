ff380c4df38e22e0e53c0568f0ffed9a2eac372a
houlai <673956297@qq.com>
2022-04-17 16:43:05 +0800

    hot fix commit

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -3,5 +3,6 @@ public class HelloGit {
         System.out.println("HelloGIt");
         System.out.println("HelloGIt2");
         System.out.println("HelloGIt3");
+        System.out.println("hot fix test");
     }
 }
